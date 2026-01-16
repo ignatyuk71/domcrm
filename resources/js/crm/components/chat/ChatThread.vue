e4f8b4a785d1491d3329f0ed18ac6b91a65ec259
Mykhailo <ignatyuk71@gmail.com>
2026-01-16 01:16:50 +0100

    куекцекцук

--- resources/js/crm/components/chat/ChatThread.vue
+++ resources/js/crm/components/chat/ChatThread.vue
@@ -23,7 +23,7 @@
             v-for="msg in messages"
             :key="msg.id || msg.temp_id"
             :message="msg"
-            :is-mine="msg.direction === 'outbound' || !msg.is_from_customer"
+            :is-mine="msg.direction === 'outbound'"
           />
         </TransitionGroup>
       </div>
