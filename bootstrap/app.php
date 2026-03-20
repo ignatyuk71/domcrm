b7e04a363555afae5a506ff8728ef542468d70a3
Mykhailo <ignatyuk71@gmail.com>
2026-03-20 12:01:25 +0100

    Додано примусове оновлення профілів чату

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         \App\Console\Commands\SyncDeliveryStatuses::class,
         \App\Console\Commands\FiscalizeDeliveredOrders::class,
         \App\Console\Commands\ReleaseStalePackingOrders::class,
+        \App\Console\Commands\RefreshChatContactProfiles::class,
     ])
     ->withMiddleware(function (Middleware $middleware) {
         // Вимикаємо перевірку CSRF для маршрутів генерації ТТН
