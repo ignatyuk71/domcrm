5086c7cef6e6e25f48f10c0ea8f7d9c961af25f9
Mykhailo <ignatyuk71@gmail.com>
2026-03-22 01:30:08 +0100

    Виправлено Bootstrap API для toast та AJAX у базі знань AI

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,9 @@
 import './bootstrap';
+import * as bootstrap from 'bootstrap';
 
 // Стилі
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-// !!! ОСЬ ЦЕЙ РЯДОК ГОЛОВНИЙ !!!
-// Ми беремо готовий файл, де вже все включено
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// Робимо Bootstrap API доступним для inline-скриптів у Blade.
+window.bootstrap = bootstrap;
